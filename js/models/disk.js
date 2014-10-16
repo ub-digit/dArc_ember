8810fb8a5ad6b6ea20b1b671962be17e49a8a9b7
Leonard Norberg <leonard.norberg@sunstone.se>
2014-10-16 11:37:44 +0200

    Added diskimage model

--- js/models/disk.js
+++ js/models/disk.js
@@ -1,5 +1,6 @@
 App.Disk = DS.Model.extend({
   pid: DS.attr('string'),
   title: DS.attr('string'),
-  archives: DS.hasMany('archive', {async: true})
+  archives: DS.hasMany('archive', {async: true}),
+  diskimages: DS.hasMany('diskimage', {async: true})
 });
