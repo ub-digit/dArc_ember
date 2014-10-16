c2803198fe7e93aa44e883e825ec04b307fac51e
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2014-10-16 18:20:59 +0200

    Added onfirmation box and fixed rollback issue

--- js/controllers/archives/show.js
+++ js/controllers/archives/show.js
@@ -2,18 +2,20 @@ App.ArchivesShowController = Ember.Controller.extend({
 	actions: {
 		deleteArchive: function() {
 			var that = this;
-     		var successHandler = function(model) {
+			var successHandler = function(model) {
 				that.transitionToRoute('archives.index');
-     		 }
+			}
 
-	      	var errorHandler = function(reason) {
-       			that.set('hasErrors', true);
-       			//that.set('errors', reason.responseJSON.errors)
-
-	        	return false;
-	      	}
-
-			that.get('model').destroyRecord().then(successHandler, errorHandler);
+			var errorHandler = function(reason) {
+				that.set('hasErrors', true);
+				that.set('errors', reason.responseJSON.errors);
+				console.log(reason);
+				that.get('model').rollback();
+				return false;
+			}
+			if(confirm("Delete?")){
+				that.get('model').destroyRecord().then(successHandler, errorHandler);
+			}
 
 		}
 	}
--- js/controllers/disks/show.js
+++ js/controllers/disks/show.js
@@ -3,16 +3,18 @@ App.DisksShowController = Ember.Controller.extend({
 		deleteDisk: function() {
 			var that = this;
 
-      		var successHandler = function(model) {
+			var successHandler = function(model) {
 				that.transitionToRoute('archives.index');
-     		 }
+			}
 
-	      	var errorHandler = function(reason) {
-       			that.set('hasErrors', true);
-	        	return false;
-	      	}
-
-			that.get('model').destroyRecord().then(successHandler, errorHandler);
+			var errorHandler = function(reason) {
+				that.set('hasErrors', true);
+				that.get('model').rollback();
+				return false;
+			}
+			if (confirm("Delete")){
+				that.get('model').destroyRecord().then(successHandler, errorHandler);
+			}
 		},
 		openContentBrowser: function(diskImage) {
 			window.open('/tree/#'+diskImage.get('id'), '_blank');
